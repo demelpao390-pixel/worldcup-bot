ec886149baed7d84396ace7df48586c7bd6fb1b8
demelpao390-pixel <demelpao390@gmail.com>
2026-06-20 16:24:50 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} está online!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(TOKEN)
